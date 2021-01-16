371f147884fa21dfca8396125e5b5cba0de70d6a
pranit <pranit27012000>
2021-01-16 18:13:33 +0530

    intialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+
+const config={
+  apiKey: "AIzaSyBioiz8ucqOs4Hg6QpulEH8AqWtGITeSe8",
+  authDomain: "chat-web-app-6167d.firebaseapp.com",
+  projectId: "chat-web-app-6167d",
+  storageBucket: "chat-web-app-6167d.appspot.com",
+  messagingSenderId: "197173785902",
+  appId: "1:197173785902:web:0612f35668acab8fa4f6bf"
+};
+
+// eslint-disable-next-line no-unused-vars
+const app = firebase.initializeApp(config);
