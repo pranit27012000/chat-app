28fedecf6f6e29607c05d21b6e9eb0af3e58011b
pranit <pranit27012000>
2021-01-17 19:11:51 +0530

    added usemediaquery hook and made drawer responsive

--- src/components/dashboard/DashboardToogle.js
+++ src/components/dashboard/DashboardToogle.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Button, Drawer, Icon } from 'rsuite'
 import Dashboard from '.'
-import { useModalState } from '../../misc/custom-hooks'
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks'
 
 const DashboardToogle = () => {
 
-  const {isOpen,open,close} = useModalState()
+  const {isOpen,open,close} = useModalState();
+  const isMobile = useMediaQuery('(max-width:992 px)');
 
   return(
     <>
     <Button block color="blue" onClick={open}>
       <Icon icon="dashboard"/> Dashboard
     </Button>
-    <Drawer show={isOpen} onHide={close} placement="left">
+    <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
     <Dashboard/>
     </Drawer>
       
